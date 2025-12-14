cffc0b80757a359e506eb40c15f91dadfe23e039
Julian <199536336+julian-bohrquez12@users.noreply.github.com>
2025-12-14 14:03:44 -0500

    reporte listo

--- backend/src/routes/reporteventas.routes.js
+++ backend/src/routes/reporteventas.routes.js
@@ -7,23 +7,26 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const [rows] = await pool.query(`
-      SELECT
-        rv.Id_Venta,
+      SELECT 
+        r.Id_Reporte,
         p.Nombre AS Producto,
         p.Precio,
         rv.Cantidad,
         rv.Fecha AS Fecha_Venta,
-        mp.Nombre AS MetodoPago
-      FROM Registro_Ventas rv
-      JOIN Productos p ON rv.Id_Productos = p.Id_Productos
-      JOIN Metodo_Pago mp ON rv.Id_Metodo = mp.Id_Metodo
+        mp.Nombre AS Metodo_Pago
+      FROM Reporte_ventas r
+      INNER JOIN Registro_Ventas rv ON r.Id_Venta = rv.Id_Venta
+      INNER JOIN Productos p ON rv.Id_Productos = p.Id_Productos
+      INNER JOIN Metodo_Pago mp ON rv.Id_Metodo = mp.Id_Metodo
       ORDER BY rv.Fecha DESC
     `);
 
     res.json(rows);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Error obteniendo reporte de ventas" });
   }
 });
+
 
 export default router;
